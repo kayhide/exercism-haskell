5db33e3a7dfd91f24a50014085a648020bfb81f3
kayhide <kayhide@gmail.com>
2019-06-18 01:11:06 +0900

    refactor haskell space-age.

--- space-age/src/SpaceAge.hs
+++ space-age/src/SpaceAge.hs
@@ -2,12 +2,15 @@ module SpaceAge (Planet(..), ageOn) where
 
 data Planet = Earth | Mercury | Venus | Mars | Jupiter | Saturn | Uranus | Neptune
 
+ageOnEarth :: Float -> Float
+ageOnEarth = (/ 31557600)
+
 ageOn :: Planet -> Float -> Float
-ageOn Earth seconds = seconds / 31557600
-ageOn Mercury seconds = ageOn Earth seconds / 0.2408467
-ageOn Venus seconds = ageOn Earth seconds / 0.61519726
-ageOn Mars seconds = ageOn Earth seconds / 1.8808158
-ageOn Jupiter seconds = ageOn Earth seconds / 11.862615
-ageOn Saturn seconds = ageOn Earth seconds / 29.447498
-ageOn Uranus seconds = ageOn Earth seconds / 84.016846
-ageOn Neptune seconds = ageOn Earth seconds / 164.79132
+ageOn Earth   = ageOnEarth
+ageOn Mercury = ageOnEarth . (/ 0.2408467)
+ageOn Venus   = ageOnEarth . (/ 0.61519726)
+ageOn Mars    = ageOnEarth . (/ 1.8808158)
+ageOn Jupiter = ageOnEarth . (/ 11.862615)
+ageOn Saturn  = ageOnEarth . (/ 29.447498)
+ageOn Uranus  = ageOnEarth . (/ 84.016846)
+ageOn Neptune = ageOnEarth . (/ 164.79132)
