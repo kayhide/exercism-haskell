2d51bdd1104f7c06fa58521aa1339057a6840446
kayhide <kayhide@gmail.com>
2019-06-27 01:21:44 +0900

    update haskell space-age.

--- space-age/src/SpaceAge.hs
+++ space-age/src/SpaceAge.hs
@@ -2,15 +2,23 @@ module SpaceAge (Planet(..), ageOn) where
 
 data Planet = Earth | Mercury | Venus | Mars | Jupiter | Saturn | Uranus | Neptune
 
-ageOnEarth :: Float -> Float
-ageOnEarth = (/ 31557600)
+newtype EarthYear = EarthYear Float
 
-ageOn :: Planet -> Float -> Float
-ageOn Earth   = ageOnEarth
-ageOn Mercury = ageOnEarth . (/ 0.2408467)
-ageOn Venus   = ageOnEarth . (/ 0.61519726)
-ageOn Mars    = ageOnEarth . (/ 1.8808158)
-ageOn Jupiter = ageOnEarth . (/ 11.862615)
-ageOn Saturn  = ageOnEarth . (/ 29.447498)
-ageOn Uranus  = ageOnEarth . (/ 84.016846)
-ageOn Neptune = ageOnEarth . (/ 164.79132)
+type Seconds = Float
+type PlanetYear = Float
+
+getSeconds :: EarthYear -> Seconds
+getSeconds (EarthYear x) = x * 60 * 60 * 24 * 365.25
+
+orbitalPeriodOf :: Planet -> EarthYear
+orbitalPeriodOf Earth   = EarthYear 1.0
+orbitalPeriodOf Mercury = EarthYear 0.2408467
+orbitalPeriodOf Venus   = EarthYear 0.61519726
+orbitalPeriodOf Mars    = EarthYear 1.8808158
+orbitalPeriodOf Jupiter = EarthYear 11.862615
+orbitalPeriodOf Saturn  = EarthYear 29.447498
+orbitalPeriodOf Uranus  = EarthYear 84.016846
+orbitalPeriodOf Neptune = EarthYear 164.79132
+
+ageOn :: Planet -> Seconds -> PlanetYear
+ageOn planet = (/ getSeconds (orbitalPeriodOf planet))
