8d93bb83fdf10a7ed06a3f81ea25fc0a8597cd12
kayhide <kayhide@gmail.com>
2019-08-13 08:05:36 +0900

    refactor haskell collatz-conjecture.

--- collatz-conjecture/src/CollatzConjecture.hs
+++ collatz-conjecture/src/CollatzConjecture.hs
@@ -1,11 +1,13 @@
 module CollatzConjecture (collatz) where
 
+import Data.List (elemIndex)
+
 collatz :: Integer -> Maybe Integer
 collatz x
-  | x > 0 = Just . fromIntegral . length . takeWhile (/= 1) $ iterate run x
+  | x > 0 = fmap fromIntegral . elemIndex 1 . iterate step $ x
   | otherwise = Nothing
 
-run :: Integer -> Integer
-run x
+step :: Integer -> Integer
+step x
   | even x = x `div` 2
   | otherwise = 3 * x + 1
