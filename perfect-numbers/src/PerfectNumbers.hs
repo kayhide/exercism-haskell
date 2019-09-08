602533e3eced08313822d92e403c7289819b1647
kayhide <kayhide@gmail.com>
2019-09-08 17:52:57 +0900

    solve haskell perfect-numbers.

---
+++ perfect-numbers/src/PerfectNumbers.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE LambdaCase #-}
+module PerfectNumbers (classify, Classification(..)) where
+
+data Classification = Deficient | Perfect | Abundant deriving (Eq, Show)
+
+classify :: Int -> Maybe Classification
+classify x = toClassification . compare x . sum <$> factors x
+  where
+    toClassification :: Ordering -> Classification
+    toClassification = \case
+      LT -> Abundant
+      EQ -> Perfect
+      GT -> Deficient
+
+factors :: Int -> Maybe [Int]
+factors x
+  | x > 0 = Just $ filter ((== 0) . mod x) [1 .. (x - 1)]
+  | otherwise = Nothing
