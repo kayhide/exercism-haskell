ac95c41ba8fa65665add93a30dd32c41346a9212
kayhide <kayhide@gmail.com>
2019-09-18 23:45:11 +0900

    refactor haskell rna-transcription.

--- rna-transcription/src/DNA.hs
+++ rna-transcription/src/DNA.hs
@@ -1,9 +1,15 @@
+{-# LANGUAGE LambdaCase #-}
 module DNA (toRNA) where
 
+import Control.Applicative
+
 toRNA :: String -> Either Char String
-toRNA "" = Right ""
-toRNA ('C' : xs) = ('G' :) <$> toRNA xs
-toRNA ('G' : xs) = ('C' :) <$> toRNA xs
-toRNA ('T' : xs) = ('A' :) <$> toRNA xs
-toRNA ('A' : xs) = ('U' :) <$> toRNA xs
-toRNA (x : _) = Left x
+toRNA = foldr (liftA2 (:)) (Right "") . fmap turn'
+
+turn' :: Char -> Either Char Char
+turn' = \case
+  'C' -> Right 'G'
+  'G' -> Right 'C'
+  'T' -> Right 'A'
+  'A' -> Right 'U'
+  x  -> Left x
