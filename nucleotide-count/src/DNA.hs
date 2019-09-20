c7b0edd8d3e06ecc218a1e0b9c40ff4b77f1ec1b
kayhide <kayhide@gmail.com>
2019-09-20 12:26:13 +0900

    refactor haskell nucleotide-count.

--- nucleotide-count/src/DNA.hs
+++ nucleotide-count/src/DNA.hs
@@ -1,26 +1,27 @@
-{-# LANGUAGE LambdaCase    #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE TupleSections #-}
 module DNA (nucleotideCounts, Nucleotide(..)) where
 
+import Data.Bifunctor (first)
 import Data.Map (Map)
 import qualified Data.Map as Map
 
 data Nucleotide = A | C | G | T deriving (Eq, Ord, Show)
 
 nucleotideCounts :: String -> Either String (Map Nucleotide Int)
-nucleotideCounts xs = maybe (Left xs) (Right . accumulate) $ traverse fromChar xs
+nucleotideCounts xs = first (const xs) $ accumulate <$> traverse fromChar xs
 
 accumulate :: [Nucleotide] -> Map Nucleotide Int
-accumulate = foldr (flip (Map.insertWith (+)) 1) empty
+accumulate = foldr (\k -> Map.insertWith (+) k 1) empty
   where
     empty :: Map Nucleotide Int
     empty = Map.fromList $ (,0) <$> [A, C, G, T]
 
-fromChar :: Char -> Maybe Nucleotide
+fromChar :: Char -> Either Char Nucleotide
 fromChar = \case
-  'A' -> Just A
-  'C' -> Just C
-  'G' -> Just G
-  'T' -> Just T
-  _ -> Nothing
+  'A' -> Right A
+  'C' -> Right C
+  'G' -> Right G
+  'T' -> Right T
+  c -> Left c
 
