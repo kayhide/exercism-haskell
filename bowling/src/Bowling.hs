610f194d61c0669e2dbd45b3c050af14b42742f1
kayhide <kayhide@gmail.com>
2019-09-30 01:01:11 +0900

    refactor haskell bowling.

--- bowling/src/Bowling.hs
+++ bowling/src/Bowling.hs
@@ -11,24 +11,38 @@ data BowlingError = IncompleteGame
   deriving (Eq, Show)
 
 score :: [Int] -> Either BowlingError Int
-score rolls = finalize =<< foldM roll (Game [] []) rolls
+score rolls = finalize =<< foldM roll (Game [] []) (zip rolls [0..])
 
-type Frame = [Int]
+
+data Frame = Open [Int] | Spare [Int] | Strike
+  deriving (Eq, Show)
+
+toRolls :: Frame -> [Int]
+toRolls = \case
+  Open ns  -> ns
+  Spare ns -> ns
+  Strike   -> pure 10
+
+fromRolls :: [Int] -> Frame
+fromRolls xs = case xs of
+  [_, _] -> bool Spare Open (sum xs < 10) xs
+  [10]   -> Strike
+  _      -> Open xs
 
 data Game =
-  Game { current :: Frame, frames :: [Frame] }
+  Game { current :: [Int], frames :: [Frame] }
   deriving (Eq, Show)
 
 
-roll :: Game -> Int -> Either BowlingError Game
-roll Game {..} i = do
+roll :: Game -> (Int, Int) -> Either BowlingError Game
+roll Game {..} (i, index) = do
   let pins = ((30 - sum current - 1) `mod` 10) + 1
   when (i < 0 || pins < i || length frames > 9) $
-    Left $ InvalidRoll (length . concat $ current : frames) i
+    Left $ InvalidRoll index i
 
   let current' = i : current
   let cont = pure $ Game current' frames
-  let next = pure $ Game [] (current' : frames)
+  let next = pure $ Game [] $ fromRolls (reverse current') : frames
   case length frames of
     9 -> case current' of
       [_, _, _] -> next
@@ -43,12 +57,12 @@ roll Game {..} i = do
 finalize :: Game -> Either BowlingError Int
 finalize Game {..} = do
   when (length frames < 10) $ Left IncompleteGame
-  pure $ calc $ reverse <$> reverse frames
+  pure $ calc $ reverse frames
   where
     calc :: [Frame] -> Int
     calc = \case
       [] -> 0
-      xs : xss -> calc xss + case (xs, sum xs) of
-        ([_], 10)    -> 10 + (sum . take 2 $ concat xss)
-        ([_, _], 10) -> 10 + (sum . take 1 $ concat xss)
-        (_, s)       -> s
+      x : xs -> calc xs + case x of
+        Open ns -> sum ns
+        Spare _ -> 10 + (sum . take 1 $ concatMap toRolls xs)
+        Strike  -> 10 + (sum . take 2 $ concatMap toRolls xs)
