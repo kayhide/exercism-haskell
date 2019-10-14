1445836b58837a678d80a55d36dad36e1db31ad2
kayhide <kayhide@gmail.com>
2019-10-14 12:28:57 +0900

    refactor haskell prime-factors.

--- prime-factors/src/PrimeFactors.hs
+++ prime-factors/src/PrimeFactors.hs
@@ -5,11 +5,15 @@ import Data.List (unfoldr)
 
 
 primeFactors :: Integer -> [Integer]
-primeFactors x = unfoldr f (2, x)
+primeFactors x = unfoldr f (possiblePrimes, x)
   where
-    f :: (Integer, Integer) -> Maybe (Integer, (Integer, Integer))
-    f (n, x') = do
+    f :: ([Integer], Integer) -> Maybe (Integer, ([Integer], Integer))
+    f ([], _) = error "Bad divisors"
+    f (n : ns, x') = do
       guard $ n <= x'
       case x' `divMod` n of
-        (x'', 0) -> pure (n, (n, x''))
-        _        -> f ((n + 1), x')
+        (x'', 0) -> pure (n, (n : ns, x''))
+        _        -> f (ns, x')
+
+possiblePrimes :: [Integer]
+possiblePrimes = 2 : 3 : ([pred, succ] <*> [6, 12 ..])
