2fb0ac8cf02df5d0612df15319ee5f581ad6923e
kayhide <kayhide@gmail.com>
2019-11-05 10:16:32 +0900

    update haskell queen-attack.

--- queen-attack/src/Queens.hs
+++ queen-attack/src/Queens.hs
@@ -1,22 +1,24 @@
 module Queens (boardString, canAttack) where
 
+import Data.Function (on)
+
 boardString :: Maybe (Int, Int) -> Maybe (Int, Int) -> String
 boardString white black = unlines $ do
   y <- [0 .. 7]
   pure . unwords $ do
     x <- [0 .. 7]
-    let pos = (y, x)
-    case (pure pos == white, pure pos == black) of
+    let pos = pure (y, x)
+    case (pos == white, pos == black) of
       (True, _) -> pure "W"
       (_, True) -> pure "B"
       _         -> pure "_"
 
 
 canAttack :: (Int, Int) -> (Int, Int) -> Bool
-canAttack (y, x) (y', x') =
-  or
-  [ y' == y
-  , x' == x
-  , y' - y == x' - x
-  , y' - y == - (x' - x)
-  ]
+canAttack a b =
+  or $ on (==) <$>
+  [ fst                         -- Same row
+  , snd                         -- Same column
+  , uncurry (+)                 -- Same diagonal /
+  , uncurry (-)                 -- Same diagonal \
+  ] <*> pure a <*> pure b
