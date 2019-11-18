c693b52754af2035263fe5970e6269ccc3045279
kayhide <kayhide@gmail.com>
2019-11-18 19:37:31 +0900

    refactor haskell isogram.

--- isogram/src/Isogram.hs
+++ isogram/src/Isogram.hs
@@ -1,7 +1,7 @@
 module Isogram (isIsogram) where
 
 import Control.Monad (foldM, guard)
-import Data.Bits (bit, zeroBits, (.|.))
+import Data.Bits (zeroBits, testBit, setBit)
 import Data.Char (isAlpha, isAscii, ord, toLower)
 import Data.Maybe (isJust)
 
@@ -9,11 +9,8 @@ isIsogram :: String -> Bool
 isIsogram =
   isJust
   . foldM f zeroBits
-  . toBits
+  . map (subtract (ord 'a') . ord . toLower)
   . filter ((&&) <$> isAlpha <*> isAscii)
   where
     f :: Int -> Int -> Maybe Int
-    f acc x = acc .|. x <$ guard (acc < acc .|. x)
-
-toBits :: String -> [Int]
-toBits = map (bit . subtract (ord 'a') . ord . toLower)
+    f acc x = setBit acc x <$ guard (not $ testBit acc x)
