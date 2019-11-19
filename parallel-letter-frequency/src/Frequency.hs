8a1534c872a939e96e147d5fc7610c8da222b2e4
kayhide <kayhide@gmail.com>
2019-11-19 11:37:28 +0900

    refactor haskell parallel-letter-frequency.

--- parallel-letter-frequency/src/Frequency.hs
+++ parallel-letter-frequency/src/Frequency.hs
@@ -2,19 +2,21 @@ module Frequency (frequency) where
 
 import Control.Parallel.Strategies (parBuffer, rseq, withStrategy)
 import Data.Char (isAlpha, toLower)
-import Data.List (foldl')
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 import Data.Text (Text)
 import qualified Data.Text as Text
 
 frequency :: Int -> [Text] -> Map Char Int
-frequency nWorkers texts = Map.unionsWith (+) $ withStrategy (parBuffer nWorkers rseq) $ build <$> texts
+frequency nWorkers texts =
+  Map.unionsWith (+)
+  $ withStrategy (parBuffer nWorkers rseq)
+  $ build <$> texts
 
 
 build :: Text -> Map Char Int
-build = foldl' f Map.empty . filter isAlpha . fmap toLower . Text.unpack
+build = Text.foldl' f Map.empty . Text.filter isAlpha
   where
     f :: Map Char Int -> Char -> Map Char Int
-    f acc c = Map.insertWith (+) c 1 acc
+    f acc c = Map.insertWith (+) (toLower c) 1 acc
 
