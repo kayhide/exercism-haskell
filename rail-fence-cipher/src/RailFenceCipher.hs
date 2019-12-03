641e90047949237bc5d5f6b80ef5f53469fa756f
Hideaki Kawai <kayhide@gmail.com>
2019-12-03 14:59:27 +0900

    refactor haskell rail-fence-cipher.

--- rail-fence-cipher/src/RailFenceCipher.hs
+++ rail-fence-cipher/src/RailFenceCipher.hs
@@ -1,50 +1,35 @@
-{-# LANGUAGE ViewPatterns #-}
 module RailFenceCipher (encode, decode) where
 
-import Control.Monad (guard)
-import Data.Bool (bool)
-import Data.List (transpose)
-import Data.Maybe (catMaybes)
+import Data.Function (on)
+import Data.List (sortBy)
+import Data.Vector (Vector)
+import qualified Data.Vector as Vector
+import qualified Data.Vector.Mutable as MVector
 
 encode :: Int -> String -> String
-encode n = concat . zipWith row [0 .. pred n] . repeat
-  where
-    row :: Int -> String -> String
-    row r = catMaybes . zipWith f (rows n)
-      where
-        f :: Int -> Char -> Maybe Char
-        f i c = c <$ guard (i == r)
+encode = go Vector.backpermute
 
 decode :: Int -> String -> String
-decode n = concat . fmap catMaybes . transpose . zipWith unrow [0 .. pred n] . chunks
+decode = go forpermute
+
+go :: (Vector Char -> Vector Int -> Vector Char) -> Int -> String -> String
+go mapper n str = Vector.toList $ mapper (Vector.fromList str) (Vector.fromList $ indices)
   where
-    chunks :: String -> [String]
-    chunks str = f counts str
-      where
-        rows' :: [Int]
-        rows' = take (length str) $ rows n
+    indices :: [Int]
+    indices = fst <$> sortBy (compare `on` snd) coords
 
-        counts :: [Int]
-        counts = (\i -> count i (rows')) <$> [0 .. pred n]
-
-        f _ []                            = []
-        f (x : xs) (splitAt x -> (s, s')) = s : f xs s'
-
-    unrow :: Int -> String -> [Maybe Char]
-    unrow r = f (rows n)
-      where
-        f _ [] = []
-        f (r' : rs) s@(c : cs) = bool (Nothing : f rs s) (pure c : f rs cs) $ r == r'
+    coords :: [(Int, Int)]
+    coords = take (length str) $ zip [0 ..] $ saw n
 
 
 -- | Generate an infinite list of:
 --   `[0, 1, ... n - 2, n - 1, n - 2, .... 1, 0, 1 ...]`
-rows :: Int -> [Int]
-rows n = f <$> [0 ..]
-  where
-    f :: Int -> Int
-    f x = bool (\y -> (n - 1) * 2 - y) id =<< (< n) $ x `mod` ((n - 1) * 2)
+saw :: Int -> [Int]
+saw n = cycle $ [0 .. n - 2] <> [n - 1, n - 2 .. 1]
 
 
-count :: Eq a => a -> [a] -> Int
-count x = length . filter (== x)
+forpermute :: Vector a -> Vector Int -> Vector a
+forpermute xs is = Vector.create $ do
+  ys <- MVector.new $ length xs
+  sequence_ $ Vector.zipWith (MVector.write ys) is xs
+  pure ys
