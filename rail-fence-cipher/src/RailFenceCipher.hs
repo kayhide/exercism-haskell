6412973d09f0bfef30fdcd2772900e316d609b9d
Hideaki Kawai <kayhide@gmail.com>
2019-12-03 15:24:52 +0900

    refactor haskell rail-fence-cipher.

--- rail-fence-cipher/src/RailFenceCipher.hs
+++ rail-fence-cipher/src/RailFenceCipher.hs
@@ -1,35 +1,18 @@
 module RailFenceCipher (encode, decode) where
 
-import Data.Function (on)
-import Data.List (sortBy)
-import Data.Vector (Vector)
-import qualified Data.Vector as Vector
-import qualified Data.Vector.Mutable as MVector
+import Data.List (sortOn)
+
 
 encode :: Int -> String -> String
-encode = go Vector.backpermute
+encode n = sortWith (saw n)
 
 decode :: Int -> String -> String
-decode = go forpermute
+decode n str = sortWith (sortWith (saw n) [0 .. length str - 1]) str
 
-go :: (Vector Char -> Vector Int -> Vector Char) -> Int -> String -> String
-go mapper n str = Vector.toList $ mapper (Vector.fromList str) (Vector.fromList $ indices)
-  where
-    indices :: [Int]
-    indices = fst <$> sortBy (compare `on` snd) coords
-
-    coords :: [(Int, Int)]
-    coords = take (length str) $ zip [0 ..] $ saw n
-
+sortWith :: [Int] -> [a] -> [a]
+sortWith is xs = snd <$> sortOn fst (zip is xs)
 
 -- | Generate an infinite list of:
 --   `[0, 1, ... n - 2, n - 1, n - 2, .... 1, 0, 1 ...]`
 saw :: Int -> [Int]
 saw n = cycle $ [0 .. n - 2] <> [n - 1, n - 2 .. 1]
-
-
-forpermute :: Vector a -> Vector Int -> Vector a
-forpermute xs is = Vector.create $ do
-  ys <- MVector.new $ length xs
-  sequence_ $ Vector.zipWith (MVector.write ys) is xs
-  pure ys
